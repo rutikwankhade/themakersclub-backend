a474deb2ac7265956805241a946f44a12ee2ff70
rutik wankhade <rutikwankhade2@gmail.com>
2021-09-11 11:35:27 +0530

    fix mongodb setup issue

--- config/db.js
+++ config/db.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose')
 
-const config = require('config');
-const db = config.get('mongoURI');
+require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(db);
+        await mongoose.connect(process.env.mongoURI);
         console.log('MongoDB connected')
 
     } catch (err) {
