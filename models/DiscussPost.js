bf084b18b50baf26356be8c7a0004c483a77597d
rutik wankhade <rutikwankhade2@gmail.com>
2021-09-15 00:58:53 +0530

    feat: create discuss post schema

---
+++ models/DiscussPost.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const DiscussPostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  
+  replies: [
+    {
+      user: {
+        type: Schema.Types.ObjectId
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('post', DiscussPostSchema);
