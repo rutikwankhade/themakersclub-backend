c4b662dee81175758e2efb70d4ad467648e11453
rutik wankhade <rutikwankhade2@gmail.com>
2021-09-14 22:53:47 +0530

    add user profile model

---
+++ models/Profile.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+
+    bio: {
+        type: String
+    },
+
+    social: {
+        github: {
+            type: String
+        },
+        twitter: {
+            type: String
+        },
+        website: {
+            type: String
+        },
+        linkedin: {
+            type: String
+        },
+
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('profile', ProfileSchema);
