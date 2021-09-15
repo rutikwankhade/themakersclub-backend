c8d185ca834984a73e49afad3c28da64af47acb6
rutik wankhade <rutikwankhade2@gmail.com>
2021-09-15 16:07:03 +0530

    :loack: fix:enable cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const connectDB = require('./config/db')
+const cors = require('cors');
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use(cors());
 
 
 //connect database
